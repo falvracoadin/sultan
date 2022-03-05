828f60672152f15fd540cd3476bfd13dfbcf58e6
nashihulibad <alamin.ibad@gmail.com>
2022-03-05 08:47:45 +0700

    update servis

--- app/Http/Livewire/Admin/Servis/CreateServis.php
+++ app/Http/Livewire/Admin/Servis/CreateServis.php
@@ -8,15 +8,31 @@ class CreateServis extends Component
 {
     public $nama_servis, $deskripsi;
 
+    public $servis;
+    public $show;
+    public $idDelete;
+
+    public $listeners = [
+        'showCreateServisPanel'
+    ];
+
     public function mount()
     {
-        //auth
+        $this->show = false;
+        $this->servis = KelolaServis::all()->toArray();
     }
-    
+
+    public function showCreateServisPanel(){
+        $this->show = true;
+    }
+     
     public function create()
     {
         //validasi
-
+        $this->validate([
+            'nama_servis' => 'required|string|max:50',
+            'deskripsi' => 'required|string',
+        ]);
        
         $servis = KelolaServis::create(
             [
@@ -26,8 +42,33 @@ class CreateServis extends Component
         );
  
         if($servis){
-            //redirect
+            $this->servis = KelolaServis::all()->toArray();
+            $this->nama_servis = '';
+            $this->deskripsi = '';
         }
+    }
+
+    public function close(){
+        $this->show = false;
+    }
+
+    public function cancel(){
+        $this->idDelete = null;
+    }
+
+    public function delete($id){
+        if($id < 0 or $id > sizeof($this->servis)) return;
+        $this->idDelete = $id;
+    }
+
+    public function confirmDelete(){
+        $id = $this->servis[$this->idDelete]['id'];
+        $status = KelolaServis::find($id)->delete();
+        if($status){
+            $this->servis = KelolaServis::all()->toArray();
+        }
+        $this->idDelete = null;
+        $this->servis = KelolaServis::all()->toArray();
     }
     
     public function render()
